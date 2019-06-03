ccad93908bf72ab6a73288fe88895c0892748d8d
yma <yemyintaung.aung@gmail.com>
2019-06-03 14:49:53 +0630

    third commit

--- app/src/main/java/com/xan/githubtest/MainActivity.java
+++ app/src/main/java/com/xan/githubtest/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    //start master
+    //start branch2
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
